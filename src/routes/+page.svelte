4a40175a09167df389881679bd0373a943d5973b
ByteAtATime <ByteAtATime@proton.me>
2023-06-11 12:41:55 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { M } from "svelte-motion";
+</script>
+
+<M.div initial="hidden" animate="show" variants={{hidden: {translateY: 200}, show: {translateY: 0}}}>
+  Hi!
+</M.div>
